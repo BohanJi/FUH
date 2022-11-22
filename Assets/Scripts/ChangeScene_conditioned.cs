466f39afaa8e42798e6a0e01a8f21db3f6b5611e
UJIjms <104430071+UJIjms@users.noreply.github.com>
2022-11-22 14:52:06 +0100

    cambio de escena condicionado

---
+++ Assets/Scripts/ChangeScene_conditioned.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene_conditioned : MonoBehaviour
+{
+	public string scene;
+    public GameObject targetObject;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        float interaction = Input.GetAxis("Interaction");
+
+        if (collision.gameObject.CompareTag("Player")){
+            if (interaction != 0)
+            {
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    if (targetObject.activeInHierarchy == false)
+                    {
+                        SceneManager.LoadScene(scene);
+                    }
+                }
+            }
+        }
+    }
+
+}
