a6e88c38f0fb6f5acf5071d720138512199f1fc6
UJIjms <104430071+UJIjms@users.noreply.github.com>
2022-11-23 17:57:39 +0100

    ScriptInventario

--- Assets/Scripts/ChangeScene_conditioned.cs
+++ Assets/Scripts/ChangeScene_conditioned.cs
@@ -15,12 +15,9 @@ public class ChangeScene_conditioned : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")){
             if (interaction != 0)
             {
-                if (collision.gameObject.CompareTag("Player"))
+                if (targetObject.activeInHierarchy == false)
                 {
-                    if (targetObject.activeInHierarchy == false)
-                    {
-                        SceneManager.LoadScene(scene);
-                    }
+                    SceneManager.LoadScene(scene);
                 }
             }
         }
