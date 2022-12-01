f99b6d8d7316c86626b622ac45fa2afb32946aa0
BohanJi <92725850+BohanJi@users.noreply.github.com>
2022-12-01 12:31:25 +0100

    juntar

--- Assets/Scripts/ArrowReached.cs
+++ Assets/Scripts/ArrowReached.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ArrowReached : MonoBehaviour
 {
-    private void OnCollisionStay2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
