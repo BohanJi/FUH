a96d8464d158ea48f7647e6bb7f3f018a6166e08
BohanJi <92725850+BohanJi@users.noreply.github.com>
2022-12-14 18:49:10 +0100

    Añadir fuente de letra y retrasar el movimiento de Brenda

--- Assets/Scripts/FollowBarry.cs
+++ Assets/Scripts/FollowBarry.cs
@@ -7,11 +7,12 @@ public class FollowBarry : MonoBehaviour
 {
     bool empezar = false;
     Queue<Vector2> posQueue = new Queue<Vector2>();
+    Vector2 lastPos = Vector2.zero;
 
     PlayerController barry;
     Rigidbody2D rBody;
-    int retraso = 150; //frame
-    // Start is call0ed before the first frame update
+    int retraso = 20;
+    // Start is called before the first frame update
     void Start()
     {
         rBody= GetComponent<Rigidbody2D>();
@@ -28,7 +29,12 @@ public class FollowBarry : MonoBehaviour
         {
             Vector2 movement;
             movement = barry.GetPosition();
-            posQueue.Enqueue(movement);
+            if(movement != lastPos) 
+            {
+                posQueue.Enqueue(movement);
+            }
+            lastPos = movement;
+            
             if (posQueue.Count > retraso)
             {
                 rBody.position = posQueue.Dequeue();
