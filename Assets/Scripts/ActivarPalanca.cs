ccb5d3519d9a4e9b404968ed5941a528d7041a8b
al408208 <al408208@uji.es>
2023-01-29 23:44:26 +0100

    fin

--- Assets/Scripts/ActivarPalanca.cs
+++ Assets/Scripts/ActivarPalanca.cs
@@ -1,14 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Collections;
+using System;
+using Unity.VisualScripting;
 
+
+    
 public class ActivarPalanca : MonoBehaviour
 {
+    
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] GameObject dialoguePanel;
+
     public InteractPalanca palanca;
     public GameObject objetActivable1;
     public GameObject objetActivable2;//palanca estado1
     public GameObject objetActivable3;//palanca estado2
     public GameObject puente2;//puente
+    int fin=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,14 +37,29 @@ public class ActivarPalanca : MonoBehaviour
             {
                 objetActivable1.SetActive(false);
                 Invoke("Espera", 1);
-                
+            }
+        }
 
+        if (interaction != 0 && fin==1)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                dialoguePanel.SetActive(false);
+                Destroy(gameObject);
             }
         }
     }
 
     void Espera(){
         objetActivable2.SetActive(true);
-        Destroy(gameObject);
+        StartDialogue();
+        fin=1;
     }
+
+    private void StartDialogue()
+    {
+        dialoguePanel.SetActive(true);
+        dialogueText.text = "Genial ya puedo activar la palanca";
+    }
+
 }
